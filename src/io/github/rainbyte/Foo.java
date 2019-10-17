e467730efa3f70752c4c10f6fb42205d356eb71e
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2019-10-17 02:23:41 -0300

    Initial commit

---
+++ src/io/github/rainbyte/Foo.java
@@ -0,0 +1,10 @@
+package io.github.rainbyte;
+
+/**
+ * Foo
+ */
+public class Foo {
+    void hello() {
+        System.out.println("Hello World from Java!");
+    }
+}
---
+++ src/io/github/rainbyte/Main.kt
@@ -0,0 +1,7 @@
+package io.github.rainbyte;
+
+fun main(args: Array<String>) {
+    val foo = Foo()
+    println("Hello World from Kotlin!")
+    foo.hello()
+}
