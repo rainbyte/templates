a291de18d468ee0f6ac1b6745753cbba3a271149
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2019-12-31 08:54:26 -0300

    javascript: add svelte template

---
+++ javascript-web-svelte/README.md
@@ -0,0 +1,21 @@
+# JavaScript Web with Svelte
+
+Usage guide
+
+1. Download deps:
+
+    ```sh
+    npm i
+    ```
+
+2. Compile:
+
+    ```sh
+    npm run build
+    ```
+
+3. Serve with reload
+
+    ```sh
+    npm run watch
+    ```
---
+++ javascript-web-svelte/src/main.svelte
@@ -0,0 +1,23 @@
+<script>
+    let clicks = 0;
+    const handleClick = () => clicks++;
+</script>
+
+<style>
+    .main {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .main * {
+        font-size: 2rem;
+    }
+</style>
+
+<div class="main">
+    <div>Number of clicks: {clicks}</div>
+    <input type="button" value="Click me" on:click={handleClick} >
+</div>
